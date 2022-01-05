33be79ec14975ded1784beb1103b785ada7fddaf
baiqiongyu <2974798592@qq.com>
2022-01-05 22:06:26 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
